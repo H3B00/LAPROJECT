889a8443fda8d9c9236383f8831cbc7cabc44143
H3B00 <H.bakker@share.epsb.ca>
2024-09-12 10:08:33 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 let mySprite = sprites.create(img`
     . . . . . . . . . . f f f . . . 
     . . . . . . . . . f f f . . . . 
-    . . . . . . f f f f f f . . . . 
-    . . . . . f f f f f f f f . . . 
-    . . . . f f f f f f f f f f . . 
-    . . . . f f f f f f f f f f . . 
-    . . . . f f f f f f f f f f . . 
+    . . . . . . f f f f 2 f . . . . 
+    . . . . . f f f f f 2 f f . . . 
+    . . . . f f f f 2 2 2 2 2 f . . 
+    . . . . f f f f f f 2 f f f . . 
+    . . . . f f f f f f 2 f f f . . 
     . f f f f f f f f f f f f f f f 
     f f f f f f f f f f f f f f f . 
     f f f f f f f f f f f f f f . . 
@@ -14,6 +14,8 @@ let mySprite = sprites.create(img`
     . f f f f f f f f f f f f f f . 
     . . f f f f f f f f f f f f . . 
     . . . f f f f f f f f f f . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . 4 . . . . 4 . . . . . 
+    . . . . 4 4 4 . . 4 4 4 . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`level2`)
